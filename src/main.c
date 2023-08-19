5286d57867b668a4aae56a6d8b3b357029c8a455
fromgreg <gregory.frommelt@c-alm.ch>
2023-08-19 21:05:28 +0200

    fixed main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]) {
 
-    /*if (argc != 3) {
+    if (argc != 3) {
         printf("Usage: %s \"h_array\"x_array\" \"x_array\"\n", argv[0]);
         return 1;
     }
@@ -12,10 +12,7 @@ int main(int argc, char *argv[]) {
     // Parse the input arguments and initialize h[] and x[]
     float h[5], x[5];
     sscanf(argv[1], "{ %f, %f, %f, %f, %f }", &h[0], &h[1], &h[2], &h[3], &h[4]);
-    sscanf(argv[2], "{ %f, %f, %f, %f, %f }", &x[0], &x[1], &x[2], &x[3], &x[4]);*/
-
-    float h[] = { 1.0, 1.0, 1.0, 1.0, 1.0 };
-    float x[] = { 1.0, 1.0, 1.0, 1.0, 1.0 };
+    sscanf(argv[2], "{ %f, %f, %f, %f, %f }", &x[0], &x[1], &x[2], &x[3], &x[4]);
 
     int lenY;
     float *y = convolve(h, x, 5, 5, &lenY);
